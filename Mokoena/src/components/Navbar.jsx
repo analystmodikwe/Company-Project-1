3504fd4074a7a3fa392fc2006642e9acc79b20bf
lesedi <analystmodikwe18@gmail.com>
2026-07-15 20:22:03 +0200

    teporarily styling the navbar, will change later

--- Mokoena/src/components/Navbar.jsx
+++ Mokoena/src/components/Navbar.jsx
@@ -1,16 +1,20 @@
-
-
-
+// navigation bar
 function Navbar() {
   return (
-    <div>
-        <h4>HOME</h4>
-        <h4>SERVICES</h4>
-        <h4>ABOUT US</h4>
-        <h4>CONTACTS</h4>
-        <h4>OUR PRODUCTS</h4>
+    <div className="text-right px-7 my-3">
+      <nav>
+        <button className="px-8  hover:bg-blue-900 text-black font-bold px-2 mx-1 rounded mb-2" >HOME</button>
+
+        <button className="px-8 hover:bg-blue-900 text-black font-bold px-2 mx-1 rounded mb-2" >SERVICES</button>
+
+        <button className="px-8 hover:bg-blue-900 text-black font-bold px-2 mx-1 rounded mb-2" >ABOUT-US</button>
+
+        <button className="px-8  hover:bg-blue-900 text-black font-bold px-2 mx-1 rounded mb-2" >CONTACTS</button>
+
+        <button className="px-8 hover:bg-blue-900 text-black font-bold px-2 mx-1 rounded mb-2" >OUR-PRODUCTS</button>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
