bc44d33e8f2f493143c84caca27e2f8ea2062949
lesedi <analystmodikwe18@gmail.com>
2026-07-15 21:58:56 +0200

    adding files

--- Mokoena/src/components/Navbar.jsx
+++ Mokoena/src/components/Navbar.jsx
@@ -3,15 +3,15 @@ function Navbar() {
   return (
     <div className="text-right px-7 my-3">
       <nav>
-        <button className="px-8  hover:bg-blue-900 text-black font-bold px-2 mx-1 rounded mb-2" >HOME</button>
+        <button className="px-8  hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2" >HOME</button>
 
-        <button className="px-8 hover:bg-blue-900 text-black font-bold px-2 mx-1 rounded mb-2" >SERVICES</button>
+        <button className="px-8 hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2" >SERVICES</button>
 
-        <button className="px-8 hover:bg-blue-900 text-black font-bold px-2 mx-1 rounded mb-2" >ABOUT-US</button>
+        <button className="px-8 hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2" >ABOUT-US</button>
 
-        <button className="px-8  hover:bg-blue-900 text-black font-bold px-2 mx-1 rounded mb-2" >CONTACTS</button>
+        <button className="px-8  hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2" >CONTACTS</button>
 
-        <button className="px-8 hover:bg-blue-900 text-black font-bold px-2 mx-1 rounded mb-2" >OUR-PRODUCTS</button>
+        <button className="px-8 hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2" >OUR-PRODUCTS</button>
       </nav>
     </div>
   );
