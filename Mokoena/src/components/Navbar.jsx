85327797891ae9f7e65f1ee8392d5767bb36f8de
lesedi <analystmodikwe18@gmail.com>
2026-07-15 19:36:53 +0200

    added the nav bar to test, befor i could add hooks

---
+++ Mokoena/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+
+function Navbar() {
+  return (
+    <div>
+        <h4>HOME</h4>
+        <h4>SERVICES</h4>
+        <h4>ABOUT US</h4>
+        <h4>CONTACTS</h4>
+        <h4>OUR PRODUCTS</h4>
+    </div>
+  )
+}
+
+export default Navbar
