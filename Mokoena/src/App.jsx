29eb2df9f29b5b31038a46974bdebd8807476a8e
lesedi <analystmodikwe18@gmail.com>
2026-07-16 02:07:08 +0200

    spacing

--- Mokoena/src/App.jsx
+++ Mokoena/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar.jsx";
-import AboutUs from "./pages/AboutUs.jsx"
+import AboutUs from "./pages/AboutUs.jsx";
 import Contact from "./pages/Contact.jsx";
 import Home from "./pages/Home.jsx";
 import Menu from "./pages/Menu.jsx";
@@ -8,19 +8,17 @@ import Footer from "./components/Footer.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
- 
-
   return (
     <div>
       {/* browser router for linking the navbar components and pages */}
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element= {<Home />} />
-          <Route path="/services" element= {<Services />} />
-          <Route path="/menu" element= {<Menu />} />
-          <Route path="/about-us" element= {<AboutUs />} />         
-          <Route path="/contacts" element= {<Contact />} />         
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contacts" element={<Contact />} />
         </Routes>
         <Footer />
       </BrowserRouter>
