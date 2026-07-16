cb9b1106823d3b02519a3ee22bf922040e73d903
lesedi <analystmodikwe18@gmail.com>
2026-07-16 20:08:47 +0200

    adding more info

--- Mokoena/src/pages/Contact.jsx
+++ Mokoena/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 // import { useNavigate } from "react-router-dom";
 import Map from "../components/Map";
 import { FaShieldHalved, FaFileLines, FaLocationDot, FaPhone, FaEnvelope } from "react-icons/fa6"; 
+import Whatsapp from "../components/Whatsapp";
 
 function Contact() {
     // const navigate = useNavigate();
@@ -12,7 +13,9 @@ function Contact() {
       <p dir="rtl" class="float-start"> <FaPhone /> 011 385 1438 </p>
 
       <p dir="rtl" class="float-start"> <FaEnvelope /> info@mokoenattholdings.co.za </p>
-           
+
+      <p>For more info you can order or enquire through WhatsApp</p> 
+
     </div>
   )
 }
