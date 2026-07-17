190d4044f7ffdd12f6e7d6c42d1874532a7aa96f
lesedi <analystmodikwe18@gmail.com>
2026-07-18 00:39:17 +0200

    adding special delight category styling

--- Mokoena/src/pages/Menu.jsx
+++ Mokoena/src/pages/Menu.jsx
@@ -1,7 +1,7 @@
 import { cakes } from "../data/cakesData.js";
 import { everydayTreats } from "../data/everydayData.js";
 
-// menu
+// MENU
 function Menu() {
   return (
     <div className="w-full bg-gray-200">
@@ -41,6 +41,33 @@ function Menu() {
 
             // key is for react to know which array is which it tracks the id or index of an array
             <div className="bg-gray-300 rounded-xl overflow-hidden shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-200" key={treat.name}>
+
+              {/* image per Array */}
+              <div className="aspect-square overflow-hidden">
+                <img src={treat.image} alt={treat.name} className="w-full h-full object-cover"/>
+              </div>
+
+              {/* image name and price */}
+              <div className="p-4 text-center">
+                <h3 className="text-lg font-medium">{treat.name}</h3>
+                <h4 className="text-sm font-semibold text-[#8a5a44] mt-1">{treat.price}</h4>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* SPECIAL DELIGHTS CATEGORY */}
+      <div className="max-w-6xl mx-auto px-6 py-10">
+        <h3 className="text-center text-3xl font-semibold tracking-wide mb-8 text-[#4a2c2a]">SPECIAL DELIGHTS</h3>
+
+        {/* each image card from the array lives here */}
+        {/* .map will perform a function in every original array and then return a new array with newly performed function */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+          {everydayTreats.map((treat) => (
+
+            // key is for react to know which array is which it tracks the id or index of an array
+            <div className="bg-gray-300 rounded-xl overflow-hidden shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-200" key={treat.name}>
               
               {/* image per Array */}
               <div className="aspect-square overflow-hidden">
@@ -56,6 +83,7 @@ function Menu() {
           ))}
         </div>
       </div>
+
     </div>
   );
 }
