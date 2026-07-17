af3e41d5ad25d290bc1f15f5cc8e8c21945967f2
lesedi <analystmodikwe18@gmail.com>
2026-07-17 17:29:48 +0200

    adding cursor

--- Mokoena/src/components/Navbar.jsx
+++ Mokoena/src/components/Navbar.jsx
@@ -1,56 +1,58 @@
-
 // navigation bar
 // ?. optional chain operator, it will only run if the value befor it exist else it will throw error
-// scrollIntoveiw will ensure that you dont navigate, the page scroll when veiwing,by its navbar 
+// scrollIntoveiw will ensure that you dont navigate, the page scroll when veiwing,by its navbar
 //  window.history.pushState(null, "", `/#${id}`);  updates URL, no reload
-  
+
 function Navbar() {
-
   const scrollToSection = (id) => {
-    document.getElementById(id)?.scrollIntoView({ behaviour: "smooth" })
-    window.history.pushState(null, "", `/#${id}`); 
+    document.getElementById(id)?.scrollIntoView({ behaviour: "smooth" });
+    window.history.pushState(null, "", `/#${id}`);
   };
-  
 
-  // buttons with onclick event listner for navigating to the correct page 
+  // buttons with onclick event listner for navigating to the correct page
   // type = "button" to ensure that it doesn't default to type = "submit"
   return (
-    <div className="sticky top-0 bg-white z-50 shadow text-right px-7 my-3">
-
-      <nav className="sticky top-0 bg-white z-50">
-
-        <button className="px-8  hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2"
+    <nav className="sticky  top-0 bg-white z-50 text-right">
+      <button
+        className="px-8 cursor-pointer  hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2"
         type="button"
-        onClick={() => scrollToSection ("home")} 
-        >HOME
-        </button>
+        onClick={() => scrollToSection("home")}
+      >
+        HOME
+      </button>
 
-        <button className="px-8 hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2"
+      <button
+        className="px-8 cursor-pointer hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2"
         type="button"
-        onClick={() => scrollToSection ("services")}  
-        >SERVICES
-        </button>
+        onClick={() => scrollToSection("services")}
+      >
+        SERVICES
+      </button>
 
-        <button className="px-8 hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2"
+      <button
+        className="px-8 cursor-pointer hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2"
         type="button"
-        onClick={() => scrollToSection ("menu")} 
-        >MENU 
-        </button>
+        onClick={() => scrollToSection("menu")}
+      >
+        MENU
+      </button>
 
-        <button className="px-8 hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2"
-        type="button" 
-        onClick={() => scrollToSection ("about-us")} 
-        >ABOUT-US
-        </button>
-
-        <button className="px-8  hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2"
+      <button
+        className="px-8 cursor-pointer hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2"
         type="button"
-        onClick={() => scrollToSection ("contacts")} 
-        >CONTACTS
-        </button>  
+        onClick={() => scrollToSection("about-us")}
+      >
+        ABOUT-US
+      </button>
 
-      </nav>
-    </div>
+      <button
+        className="px-8 cursor-pointer  hover:bg-blue-700 text-black font-bold px-2 mx-1 rounded mb-2"
+        type="button"
+        onClick={() => scrollToSection("contacts")}
+      >
+        CONTACTS
+      </button>
+    </nav>
   );
 }
 
