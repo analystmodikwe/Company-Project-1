1e790e7f602e0e1545552092a79360b078eef319
lesedi <analystmodikwe18@gmail.com>
2026-07-17 22:43:36 +0200

    separating the js logic with jsx

--- Mokoena/src/pages/Menu.jsx
+++ Mokoena/src/pages/Menu.jsx
@@ -1,20 +1,5 @@
-// imports for the cake category images
-import ClassicChocolate from "../images/cakes/ClassicChocolate.jpg";
-import LemonDrizzle from "../images/cakes/LemonDrizzle.jpg";
-import NutellaChocolate from "../images/cakes/NutellaChocolate.jpg";
-import RedVelvet from "../images/cakes/RedVelvet.jpg";
-import VanillaSponge from "../images/cakes/VanillaSponge.jpg";
-import WeddingCake from "../images/cakes/WeddingCake.jpg";
+import { cakes } from "../data/cakesData.js";
 
-// an array for different pictures
-const cakes = [
-  { name: "Classic Chocolate", image: ClassicChocolate, price: `R ${100}` },
-  { name: "Lemon Drizzle", image: LemonDrizzle, price: `R ${100}` },
-  { name: "Nutella Chocolate", image: NutellaChocolate, price: `R ${100}` },
-  { name: "Red Velvet", image: RedVelvet, price: `R ${100}` },
-  { name: "Vanilla Sponge ", image: VanillaSponge , price: `R ${100}` },
-  { name: "Wedding Cake's", image: ClassicChocolate, price: `from R ${100} depending on your desired style`},
-];
 
 // menu
 function Menu() {
