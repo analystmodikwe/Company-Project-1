bc073f56331e3292f2807b830a8c09b3f0ac1097
lesedi <analystmodikwe18@gmail.com>
2026-07-18 00:32:06 +0200

    adding data

--- Mokoena/src/data/everydayData.js
+++ Mokoena/src/data/everydayData.js
@@ -1,8 +1,24 @@
 // imports for the everyday treats category
 import BrownBread from "../images/everyday-treats/BrownBread.png";
-import BurgerAndHotdog from "../images/everyday-treats/BurgerAndHotdog.png"
+import BurgerAndHotdog from "../images/everyday-treats/BurgerAndHotdog.png";
+import CreamyBuns from "../images/everyday-treats/CreamyBuns.jpg";
+import GingerBiscuits from "../images/everyday-treats/GingerBiscuits.jpg";
+import RaisinBread from "../images/everyday-treats/RaisinBread.png";
+import SchoolBuns from "../images/everyday-treats/SchoolBuns.jpg";
+import Scones from "../images/everyday-treats/Scones.jpg";
+import SnowBalls from "../images/everyday-treats/SnowBalls.jpg";
+import WhiteBread from "../images/everyday-treats/WhiteBread.png";
 
-
-export const everydayTreats =[
-   { name: "Brown Bread", image: BrownBread, price: `R ${12} each` }, 
+// an array for different pictures in the everydayTreats category
+export const everydayTreats = [
+  { name: "Brown Bread", image: BrownBread, price: `R ${12} each` },
+  { name: "White Bread", image: WhiteBread, price: `R ${14} each` },
+  { name: "Burger And Hotdog Buns", image: BurgerAndHotdog, price: `R ${5} each` },
+  { name: "Creamy Buns", image: CreamyBuns, price: `R ${20} each pack` },
+  { name: "Ginger Biscuits", image: GingerBiscuits, price: `R ${18} each pack` },
+  { name: "Raisin Bread", image: RaisinBread, price: `R ${18} each` },
+  { name: "School Buns", image: SchoolBuns, price: `R ${10} each pack` },
+  { name: "Scones", image: Scones, price: `R ${15} each pack` },
+  { name: "Snowballs", image: SnowBalls, price: `R ${17} each pack` },
+  
 ];
