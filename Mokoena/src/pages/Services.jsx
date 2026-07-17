299d49c5485c2377e66ef81910d8665e16ba35a5
lesedi <analystmodikwe18@gmail.com>
2026-07-17 05:38:36 +0200

    adding style on services page

--- Mokoena/src/pages/Services.jsx
+++ Mokoena/src/pages/Services.jsx
@@ -1,14 +1,22 @@
 import ArtisanBread1 from "../images/services-images/ArtisanBread1.jpg";
+import CustomCupCakes from "../images/services-images/CustomCupCakes.jpg";
+import DelectableCakes  from "../images/services-images/DelectableCakes.jpg";
+import MeltingMoments  from "../images/services-images/MeltingMoments.jpg";
+import SeasonalSpecials  from "../images/services-images/SeasonalSpecials.jpg";
+import SpecialOccasionTreats  from "../images/services-images/SpecialOccasionTreats.jpg";
 
 
 //  an array to hold parts/images that differ
 const services = [
-  { name: "Artisan Breads", image: ArtisanBread1 }
-
+  { name: "Artisan Breads", image: ArtisanBread1 },
+  { name: "Custom Cup Cakes", image: CustomCupCakes },
+  { name: "Delectable Cakes", image: DelectableCakes },
+  { name: "Melting Moments", image: MeltingMoments },
+  { name: "Seasonal Specials", image: SeasonalSpecials },
+  { name: "Special Occasion Treats", image: SpecialOccasionTreats },
 ];
 
 // services
-
 function Services() {
   return (
     <div className="max-w-6xl mx-auto px-6 py-20">
