2b94dcec71c210be3c228497bff992565e6ef3d1
lesedi <analystmodikwe18@gmail.com>
2026-07-18 01:36:47 +0200

    prices

--- Mokoena/src/data/cakesData.js
+++ Mokoena/src/data/cakesData.js
@@ -8,10 +8,10 @@ import WeddingCake from "../images/cakes/WeddingCake.jpg";
 
 // an array for different pictures in the cake category
 export const cakes = [
-  { name: "Classic Chocolate", image: ClassicChocolate, price: `R ${100}` },
-  { name: "Lemon Drizzle", image: LemonDrizzle, price: `R ${100}` },
-  { name: "Nutella Chocolate", image: NutellaChocolate, price: `R ${100}` },
-  { name: "Red Velvet", image: RedVelvet, price: `R ${100}` },
-  { name: "Vanilla Sponge ", image: VanillaSponge , price: `R ${100}` },
-  { name: "Wedding Cake's", image: WeddingCake, price: `from R ${100} depending on your desired style`},
+  { name: "Classic Chocolate", image: ClassicChocolate, price: `R ${250} depending on your desired style and size` },
+  { name: "Lemon Drizzle", image: LemonDrizzle, price: `R ${200} depending on your desired style and size` },
+  { name: "Nutella Chocolate", image: NutellaChocolate, price: `R ${300} depending on your desired style and size` },
+  { name: "Red Velvet", image: RedVelvet, price: `R ${200} depending on your desired style and size` },
+  { name: "Vanilla Sponge ", image: VanillaSponge , price: `from R ${300} depending on your desired style and size` },
+  { name: "Wedding Cake's", image: WeddingCake, price: `from R ${1000}, depending on your desired style and size`},
 ];
