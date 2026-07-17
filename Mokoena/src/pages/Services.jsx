2dbcc01100e0d37b120cdc3721c545dbe420f6c9
lesedi <analystmodikwe18@gmail.com>
2026-07-18 01:20:05 +0200

    separating js from jsx

--- Mokoena/src/pages/Services.jsx
+++ Mokoena/src/pages/Services.jsx
@@ -1,21 +1,7 @@
-import ArtisanBread1 from "../images/services-images/ArtisanBread1.jpg";
-import CustomCupCakes from "../images/services-images/CustomCupCakes.jpg";
-import DelectableCakes from "../images/services-images/DelectableCakes.jpg";
-import MeltingMoments from "../images/services-images/MeltingMoments.jpg";
-import SeasonalSpecials from "../images/services-images/SeasonalSpecials.jpg";
-import SpecialOccasionTreats from "../images/services-images/SpecialOccasionTreats.jpg";
 
+
+// background photo 
 import Background from "../images/services-images/Background.png";
-
-//  an array to hold parts/images that differ
-const services = [
-  { name: "Artisan Breads", image: ArtisanBread1 },
-  { name: "Custom Cup Cakes", image: CustomCupCakes },
-  { name: "Delectable Cakes", image: DelectableCakes },
-  { name: "Melting Moments", image: MeltingMoments },
-  { name: "Seasonal Specials", image: SeasonalSpecials },
-  { name: "Special Occasion Treats", image: SpecialOccasionTreats },
-];
 
 // services
 function Services() {
@@ -31,7 +17,7 @@ function Services() {
       <div className="max-w-6xl mx-auto px-6 py-20">
 
         <div className="text-center mb-12">
-          
+
           <p className="text-orange-100 font-semibold tracking-wide uppercase text-sm mb-2">
             What We Offer
           </p>
