0f9ecdf0c5887334ea100a88f13c5b47fd2d299e
lesedi <analystmodikwe18@gmail.com>
2026-07-18 00:53:02 +0200

    adding data

---
+++ Mokoena/src/data/specialdelightsData.js
@@ -0,0 +1,10 @@
+import Cinamon from "../images/special-delights/Cinamon.jpg";
+import Croissants from "../images/special-delights/Croissants.jpg";
+import SourDough from "../images/special-delights/SourDough.jpg";
+
+// an array for different pictures in the everydayTreats category
+export const specialdelights = [
+    { name: "Cinamon", image: Cinamon, price: `R ${11} each` },
+    { name: "Croissants", image: Croissants, price: `R ${10} each` },
+    { name: "SourDough", image: SourDough, price: `R ${22} each` },
+];
