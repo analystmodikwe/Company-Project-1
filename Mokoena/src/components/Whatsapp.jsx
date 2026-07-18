1fc674e7f1aa612655dadb7a7a548ef81f1bc4be
lesedi <analystmodikwe18@gmail.com>
2026-07-18 22:49:48 +0200

    responsiveness debugging on whatsapp link

--- Mokoena/src/components/Whatsapp.jsx
+++ Mokoena/src/components/Whatsapp.jsx
@@ -1,6 +1,6 @@
+import { FaWhatsapp } from "react-icons/fa6";
+
 // whatsapp component for customers to be able to order
-
-
 function Whatsapp() {
   const phoneNumber = "27113851438";
   const message = "Hi, I'd like to place an order";
@@ -12,9 +12,11 @@ function Whatsapp() {
     <a href={whatsappUrl} target="_blank" 
     rel="noopener noreferrer" 
 
-    // styling for making the whatsapp link to be fixed so that it appears in every page
-    className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white font-bold px-6 py-3 rounded-full shadow-lg z-50">
-    Order Via Whatsapp
+    // styling for making the whatsapp link to be fixed so that it appears in every page the span will turn it to a circle on mobile
+    className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white font-bold py-3 rounded-full shadow-lg z-50 flex items-center gap-2 p-4 sm:px-6 sm:py-3">
+
+      <FaWhatsapp className="text-2xl sm:text-xl" />
+      <span className="hidden sm:inline">Order Via Whatsapp</span>
     </a>
   );
 }
