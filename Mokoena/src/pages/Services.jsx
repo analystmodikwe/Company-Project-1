50b7a63cf77100f21aa70df059d6d1b8eb13fa91
lesedi <analystmodikwe18@gmail.com>
2026-07-18 18:57:18 +0200

    adding blur background after deleting the picture

--- Mokoena/src/pages/Services.jsx
+++ Mokoena/src/pages/Services.jsx
@@ -1,23 +1,25 @@
 import { services } from "../data/services.js";
 
-// background photo
-import Background from "../images/services-images/Background.webp";
+
 
 // services
 function Services() {
   return (
-    <div className="relative">
-      {/* background image */}
-      <div
-        className= "absolute inset-0 bg-cover bg-center blur-md scale-110"
-        style={{ backgroundImage: `url(${Background})` }}
-      ></div>
-      {/* dark overlay so cards stay readable */}
-      <div className="absolute inset-0 bg-black/10"></div>
+
+    <div className="relative  overflow-hidden">
+
+      {/* background Blur */}
+      <div className= "absolute inset-0 -z-10">
+
+        {/* the orange blur top left and bottom left */}
+        <div className="absolute top-0 left-0 w-96 h-96 bg-orange-300 rounded-full blur-3xl opacity-40"></div>
+        <div className="absolute bottom-0 right-0 w-96 h-96 bg-orange-500 rounded-full blur-3xl opacity-30"></div> 
+
+      </div>
 
       <div className="relative max-w-6xl mx-auto px-6 py-20">
         <div className="text-center mb-12">
-          <p className="text-orange-100 font-semibold tracking-wide uppercase text-sm mb-2">
+          <p className="text-orange-700 font-semibold tracking-wide uppercase text-sm mb-2">
             What We Offer
           </p>
 
@@ -45,7 +47,7 @@ function Services() {
 
               {/* image name  */}
               <div className="p-5">
-                <h3 className="text-lg font-semibold text-gray-100 text-center">
+                <h3 className="text-lg font-semibold text-gray-700 text-center">
                   {service.name}
                 </h3>
               </div>
